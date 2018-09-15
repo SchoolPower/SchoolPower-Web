bebacdf82a6346df71c10fb8ad7e9e582357450e
Harry Null <harryyunull@gmail.com>
2018-09-15 14:21:44 +0800

    check password at login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,6 +9,9 @@
                 <v-toolbar-title>SchoolPower Login</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
+                <v-alert v-if="err" :value="true" type="error" class="mb-3">
+                  {{err['description']}}
+                </v-alert>
                 <v-form>
                   <v-text-field prepend-icon="person" v-model="username"
                                 name="login" label="Login" type="text"></v-text-field>
@@ -35,13 +38,38 @@ export default {
     return {
       username: '',
       password: '',
+      err: null,
     };
   },
   methods: {
     login() {
-      this.$cookie.set('username', this.username, { expires: '1Y' });
-      this.$cookie.set('password', this.password, { expires: '1Y' });
-      this.$router.push('/');
+      const formData = new FormData();
+      formData.append('username', this.username);
+      formData.append('password', this.password);
+      formData.append('os', 'web');
+      formData.append('version', '1.0.0');
+      formData.append('action', 'login');
+      fetch('https://api.schoolpower.tech/api/2.0/get_data.php',
+        {
+          method: 'POST',
+          body: formData,
+        })
+        .then(response => response.json())
+        .then((json) => {
+          if (json.err) {
+            this.err = json;
+            return;
+          }
+
+          localStorage.information = JSON.stringify(json.information);
+          localStorage.subjects = JSON.stringify(json.sections);
+          localStorage.attendances = JSON.stringify(json.attendances);
+          localStorage.additional = JSON.stringify(json.additional);
+          this.$cookie.set('username', this.username, { expires: '1Y' });
+          this.$cookie.set('password', this.password, { expires: '1Y' });
+          this.$router.push('/');
+        });
+
     },
   },
 };
