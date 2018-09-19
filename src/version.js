0c873fd1c615d56ebfed482ae85904c727832eef
Harry Null <harryyunull@gmail.com>
2018-09-19 20:38:12 +0800

    update

--- src/version.js
+++ src/version.js
@@ -2,7 +2,11 @@ export default {
   Version: '1.0.1',
   Updates: [
     '添加了出勤面板',
-    '修复了刷新成绩的问题',
+    '添加了成绩更新成功提示',
+    '添加了GPA',
+    '添加了本面板',
+    '修复了刷新成绩失败的问题',
+    '其它改进',
   ],
   store: {
     lastShownVersion: localStorage.lastShownVersion,
