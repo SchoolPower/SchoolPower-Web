1a1c2e8b6866b625d2c5bf760b8c9d17cd3e8e33
Harry Null <harryyunull@gmail.com>
2018-09-22 18:41:34 +0800

    hide scrollbar

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,60 +3,66 @@
     <v-container fluid fill-height class="pa-0">
       <v-layout class="fill-height" style="height: 100vh; min-height:100vh;">
         <v-flex v-bind:xs6="!$vuetify.breakpoint.xsOnly"
-                v-bind:class="{ hidden: selectedSubject && $vuetify.breakpoint.xsOnly,
-                  'pa-3': $vuetify.breakpoint.xsOnly, 'pa-5': $vuetify.breakpoint.smAndUp}"
-                class="grey lighten-4 scroll_area" style="flex: 1">
-          <h1 class="pa-3">Dashboard</h1>
-          <v-layout justify-space-between row>
-            <v-flex xs12 lg5>
-              <v-card>
-                <v-layout justify-space-between row>
-                  <v-flex xs6>
+                v-bind:class="{ hidden: selectedSubject && $vuetify.breakpoint.xsOnly}",
+                class="grey lighten-4 outer">
+          <div class="middle">
+            <div v-bind:class="{ 'pa-3': $vuetify.breakpoint.xsOnly,
+                                 'pa-5': $vuetify.breakpoint.smAndUp }"
+                 class="inner">
+              <h1 class="pa-3">Dashboard</h1>
+              <v-layout justify-space-between row>
+                <v-flex xs12 lg5>
+                  <v-card>
+                    <v-layout justify-space-between row>
+                      <v-flex xs6>
+                        <v-card-title primary-title>
+                          <h3 class="headline mb-2" style="font-size:2em">GPA</h3>
+                          <v-select :items="terms" label="Term" v-model="selectedTerm"></v-select>
+                        </v-card-title>
+                      </v-flex>
+                      <v-flex xs6 class="pa-3">
+                        <div>
+                          <v-progress-circular :rotate="360" :size="100" :width="15"
+                                               :value="parseFloat(gpa)" color="teal"
+                          >{{gpa}}
+                          </v-progress-circular>
+                        </div>
+                      </v-flex>
+                    </v-layout>
+                  </v-card>
+                </v-flex>
+                <v-flex xs12 lg6 v-if="versionStore.lastShownVersion !== version">
+                  <v-card class="pa-1">
                     <v-card-title primary-title>
-                      <h3 class="headline mb-2" style="font-size:2em">GPA</h3>
-                      <v-select :items="terms" label="Term" v-model="selectedTerm"></v-select>
+                      <h3 class="headline" style="font-size:2em">更新记录</h3>
                     </v-card-title>
-                  </v-flex>
-                  <v-flex xs6 class="pa-3">
-                    <div>
-                      <v-progress-circular :rotate="360" :size="100" :width="15"
-                                           :value="parseFloat(gpa)" color="teal"
-                      >{{gpa}}
-                      </v-progress-circular>
+                    <div class="pl-3 pb-3">
+                      <p>目前版本: {{version}}</p>
+                      <p>注意 SchoolPower Web 版仍处于 Beta 阶段，
+                        若遇到 bug 请通过邮件<a href="mailto: harryyunull@gmail.com">反馈给我们</a>。</p>
+                      <ul v-for="item in updates" v-bind:key="item">
+                        <li>{{item}}</li>
+                      </ul>
                     </div>
-                  </v-flex>
-                </v-layout>
-              </v-card>
-            </v-flex>
-            <v-flex xs12 lg6 v-if="versionStore.lastShownVersion !== version">
-              <v-card class="pa-1">
-                <v-card-title primary-title>
-                  <h3 class="headline" style="font-size:2em">更新记录</h3>
-                </v-card-title>
-                <div class="pl-3 pb-3">
-                  <p>目前版本: {{version}}</p>
-                  <p>注意 SchoolPower Web 版仍处于 Beta 阶段，
-                    若遇到 bug 请通过邮件<a href="mailto: harryyunull@gmail.com">反馈给我们</a>。</p>
-                  <ul v-for="item in updates" v-bind:key="item">
-                    <li>{{item}}</li>
-                  </ul>
-                </div>
 
-                <v-card-actions>
-                  <v-btn flat color="orange"
-                         @click="dismissUpdate()">Close</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-flex>
-          </v-layout>
-          <div class="subject_list mt-3">
-            <v-list two-line class="grey lighten-4">
-              <subject-item v-for="subject in studentInfo.subjects"
-                            v-bind:subject="subject" :key="subject.name"
-                            @subject-click="selectedSubject = subject">
-              </subject-item>
-            </v-list>
+                    <v-card-actions>
+                      <v-btn flat color="orange"
+                             @click="dismissUpdate()">Close</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-flex>
+              </v-layout>
+              <div class="subject_list mt-3">
+                <v-list two-line class="grey lighten-4">
+                  <subject-item v-for="subject in studentInfo.subjects"
+                                v-bind:subject="subject" :key="subject.name"
+                                @subject-click="selectedSubject = subject">
+                  </subject-item>
+                </v-list>
+              </div>
+            </div>
           </div>
+
         </v-flex>
 
         <v-flex v-bind:xs6="!$vuetify.breakpoint.xsOnly" class="grey lighten-4 scroll_area pl-4"
@@ -158,20 +164,33 @@ export default {
     height: 100%;
     overflow: auto;
   }
+
+  .outer{
+    flex: 1;
+    display: flex;
+    height: 100%;
+    align-items: stretch;
+  }
+
+  .middle {
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    position: relative;
+    flex-grow: 1;
+    overflow: hidden;
+    margin-left: -17px;
+  }
+
+  .inner{
+    position: relative;
+    overflow-y: auto;
+    flex-grow: 1;
+    right: -17px;
+  }
 </style>
 
-<style>html {
-  overflow: scroll;
-  overflow-x: hidden;
-}
-::-webkit-scrollbar {
-  width: 0;  /* remove scrollbar space */
-  background: transparent;  /* optional: just make scrollbar invisible */
-}
-/* optional: show position indicator in red */
-::-webkit-scrollbar-thumb {
-  background: #FF0000;
-}
+<style>
   html, body {
     overflow: hidden;
   }
