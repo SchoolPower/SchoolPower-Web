fb13ee6c6078fd7700b52178468e9ff3a2b94a8e
Harry Null <harryyunull@gmail.com>
2018-09-28 10:20:48 +0800

    1.0.5

--- src/version.js
+++ src/version.js
@@ -1,7 +1,8 @@
 export default {
-  Version: '1.0.4',
+  Version: '1.0.5',
   Updates: [
     '添加了 GPA 设置',
+    '修复了计算 GPA 时的一些 Bug',
   ],
   store: {
     lastShownVersion: localStorage.lastShownVersion,
